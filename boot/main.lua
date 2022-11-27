1068bc3ea162c462a58b306a64d9640ac174e18b
Sencres013 <77643682+Sencres013@users.noreply.github.com>
2022-11-27 15:29:45 +0100

    Delete main.lua

--- boot/main.lua
+++
@@ -1,8 +0,0 @@
-while true do end
-local gpu = component.proxy(component.list("gpu")());
-
-gpu.setDepth(1);
-gpu.setBackground(0);
-gpu.setForeground(1);
-local x, y = gpu.getResolution();
-gpu.fill(1, 1, x, y, "A");
