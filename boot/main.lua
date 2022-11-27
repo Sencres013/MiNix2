3901aafa020274273c3ca8a5d5e8ba1e139acaec
Sencres013 <77643682+Sencres013@users.noreply.github.com>
2022-11-27 10:39:04 +0100

    Add files via upload

--- boot/main.lua
+++ boot/main.lua
@@ -1,9 +1,8 @@
-local gpu = component.proxy(component.list("gpu", true));
+while true do end
+local gpu = component.proxy(component.list("gpu")());
 
 gpu.setDepth(1);
 gpu.setBackground(0);
 gpu.setForeground(1);
 local x, y = gpu.getResolution();
 gpu.fill(1, 1, x, y, "A");
-
-while true do end
