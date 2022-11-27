9a52ab7eefddde4a1fc4cf8629ff7c1d3099aa0a
Sencres013 <77643682+Sencres013@users.noreply.github.com>
2022-11-27 16:00:30 +0100

    Add files via upload

--- boot/EFI/bios.lua
+++ boot/EFI/bios.lua
@@ -1,16 +1,3 @@
---[[do
-    local deadline = computer.uptime() + 1;
-
-    while computer.uptime() < deadline do
-        local signal = table.pack(computer.pullSignal(1));
-
-        -- lshift or rshift key held down
-        if signal[1] == "key_down" and (signal[4] == 0x2a or signal[4] == 0x36) then
-            
-        end
-    end
-end]]
-
 do
     local gpu = component.proxy(component.list("gpu")());
     local screen = component.proxy(component.list("screen")());
@@ -22,16 +9,9 @@ do
     end
 end
 
-
 local eeprom = component.proxy(component.list("eeprom")());
-error("a: " .. eeprom.getData(), 0);
-do
 
-    computer.getBootAddress = computer.getBootAddress or function() return eeprom.getData() end;
-    computer.setBootAddress = computer.setBootAddress or function(data) return eeprom.setData(data) end;
-end
-
-local main;
+local init;
 
 do
     local bootFs, fsList, index = component.proxy(eeprom.getData()), {}, 1;
@@ -39,41 +19,43 @@ do
     if not bootFs then
         local tempIndex = 1;
         for address in component.list("filesystem") do
-            fsList[tempIndex] = address;
-            tempIndex = tempIndex + 1;
+            if component.invoke(address, "getLabel") ~= "tmpfs" then
+                fsList[tempIndex] = address;
+                tempIndex = tempIndex + 1;
+            end
         end
 
         bootFs = component.proxy(fsList[1]);
         index = 2;
     end
-
+    
     while bootFs do
-        if bootFs.exists("/boot/main.lua") then
-            local handle = bootFs.open("/boot/main.lua");
-
+        if bootFs.exists("/sbin/init.lua") then
+            local handle = bootFs.open("/sbin/init.lua");
+            
             local buffer, data = "", "";
             repeat
                 data = data .. buffer;
                 buffer = bootFs.read(handle, math.huge);
             until not buffer
-
-            local _main, err = load(data, "=main");
-
-            if _main == "fail" then
+            
+            local _init, err = load(data, "=init");
+            
+            if _init == "fail" then
                 error("Error loading OS: " .. err);
             end
-
-            main = _main;
+            
+            init = _init;
             break;
         end
-
+        
         bootFs = component.proxy(fsList[index]);
         index = index + 1;
     end
 end
 
-if not main then
+if not init then
     error("no bootable medium found");
 end
 
-main();
+init();
