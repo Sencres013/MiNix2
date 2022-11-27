8d2c119b2675c6f67e3878306c4cf916493ad94f
Sencres013 <77643682+Sencres013@users.noreply.github.com>
2022-11-27 16:08:45 +0100

    Add files via upload

--- sbin/init.lua
+++ sbin/init.lua
@@ -1,1 +1,9 @@
+local gpu = component.proxy(component.list("gpu")());
+gpu.setDepth(1);
+gpu.setBackground(0);
+gpu.setForeground(0);
+local x, y = gpu.getResolution();
+gpu.fill(1, 1, x, y, " ");
+gpu.setForeground(1);
+gpu.set(1, 1, "cool beans");
 while true do end
